9d1cbb2e2dca32d5300062a4fd6cb438f64a4b70
Angel Petrov <64444672+Petrozza@users.noreply.github.com>
2020-10-29 02:56:57 +0200

    Revert "add"
    This reverts commit 77b4be96dd7eebdddcd4a1ab0dd6b2d8c2d08fa1.

---
+++ MOBA Challenger/Snowwhite/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace Snowwhite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dwarfs = new Dictionary<string, Dictionary<string, int>>();
+            string input = Console.ReadLine();
+            while (input != "Once upon a time")
+            {
+                string[] data = input.Split(" <:> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = data[0];
+                string color = data[1];
+                int physics = int.Parse(data[2]);
+
+                if (!dwarfs.ContainsKey(color))
+                {
+                    dwarfs.Add(color, new Dictionary<string, int>());
+                    if (!dwarfs[color].ContainsKey(name))
+                    {
+                        dwarfs[color].Add(name, physics);
+                    }
+                    else
+                    {
+                        dwarfs[color][name] = physics;
+                    }
+                }
+                else
+                {
+                    if (!dwarfs[color].ContainsKey(name))
+                    {
+                        dwarfs[color].Add(name, physics);
+                    }
+                }
+
+                if (dwarfs[color][name] < physics)
+                {
+                    dwarfs[color][name] = physics;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var sortedDwarfs = new Dictionary<string, int>();
+
+            foreach (var color in dwarfs.OrderByDescending(x => x.Value.Count))
+            {
+                foreach (var item in color.Value)
+                {
+                    sortedDwarfs.Add($"({color.Key}) {item.Key} <->", item.Value);
+                }
+            }
+            foreach (var pair in sortedDwarfs.OrderByDescending(v => v.Value))
+            {
+                Console.WriteLine($"{pair.Key}{pair.Value}");
+            }
+        }
+    }
+}
